83c154c00417f2d41fe8abc96ac074e17679d3bf
Andrei Mesilov <dronoldino@gmail.com>
2023-10-20 13:17:45 +0500

    task3

--- src/main/java/ru/naumen/collection/task3/WarAndPeace.java
+++ src/main/java/ru/naumen/collection/task3/WarAndPeace.java
@@ -1,6 +1,7 @@
 package ru.naumen.collection.task3;
 
 import java.nio.file.Path;
+import java.util.*;
 
 /**
  * <p>Написать консольное приложение, которое принимает на вход произвольный текстовый файл в формате txt.
@@ -18,6 +19,45 @@ public class WarAndPeace {
             "Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt");
 
     public static void main(String[] args) {
-        // TODO
+        Scanner in = new Scanner(System.in);
+        String inputPath = in.nextLine();
+        Path txtPath;
+        if (inputPath.isEmpty()) {
+            txtPath = WAR_AND_PEACE_FILE_PATH;
+        } else {
+            txtPath = Path.of(inputPath);
+        }
+
+
+        WordParser wp = new WordParser(txtPath);
+
+        HashMap<String, Integer> wordCounter = countWords(wp);
+
+        List<Map.Entry<String, Integer>> entryList = wordCounter
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .toList();
+
+        List<Map.Entry<String, Integer>> top10 = entryList.
+                subList(0, Math.min(entryList.size(), 10));
+
+        List<Map.Entry<String, Integer>> least10 = entryList
+                .subList(entryList.size()-Math.min(entryList.size(), 10),
+                        entryList.size());
+        System.out.println(top10);
+        System.out.println(least10);
+    }
+
+    public static HashMap<String, Integer>  countWords(WordParser wp) {
+        HashMap<String, Integer> wordCounter = new HashMap<>();
+        wp.forEachWord(w -> {
+            if (!wordCounter.containsKey(w)) {
+                wordCounter.put(w, 1);
+            } else {
+                wordCounter.put(w, wordCounter.get(w) + 1);
+            }
+        });
+        return wordCounter;
     }
 }
