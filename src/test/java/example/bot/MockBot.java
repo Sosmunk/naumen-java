e7db9c3ae243fc595d4a8ce921fbc1f5572f509d
Andrei Mesilov <dronoldino@gmail.com>
2023-12-09 23:56:09 +0500

    javadoc

--- src/test/java/example/bot/MockBot.java
+++ src/test/java/example/bot/MockBot.java
@@ -3,14 +3,24 @@ package example.bot;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Моковая реализация бота
+ */
 public class MockBot implements Bot{
 
+    /**
+     * Сообщения отправленные пользователю
+     */
     private final List<String> messages = new ArrayList<>();
     @Override
     public void sendMessage(Long chatId, String message) {
         messages.add(message);
     }
 
+    /**
+     * Получить отправленные сообщения
+     * @return сообщения
+     */
     public List<String> getMessages() {
         return messages;
     }
